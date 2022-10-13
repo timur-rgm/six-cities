f1b88aa6bc62731e6cbcf5f3ae1862f99c9153a9
Timur <tiabumuaz@gmail.com>
2022-10-13 16:28:06 +0500

    Добавлен инструмент отладки redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
@@ -11,7 +12,10 @@ const Setting = {
   PLACES_COUNT: 5,
 };
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
